869bc84ac6f2ae8731d72d775109c174427d4bf9
aminatabah-work <aminata.bah.work@gmail.com>
2026-03-17 22:23:00 -0500

    Use dynamic tenant_id instead of hardcoding

--- main.tf
+++ main.tf
@@ -13,6 +13,8 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "rg" {
   name     = "rg-keyvault-lab"
   location = var.location
@@ -22,7 +24,7 @@ resource "azurerm_key_vault" "kv" {
   name                       = var.keyvault_name
   location                   = azurerm_resource_group.rg.location
   resource_group_name        = azurerm_resource_group.rg.name
-  tenant_id                  = "00000000-0000-0000-0000-000000000000"
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard"
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
