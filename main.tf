90f0470bf4f12c50b1cd71790f2f6826517b45c2
aminatabah-work <aminata.bah.work@gmail.com>
2026-03-17 22:07:21 -0500

    Add Azure Key Vault and secret resources

--- main.tf
+++ main.tf
@@ -12,3 +12,24 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-keyvault-lab"
+  location = var.location
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                       = var.keyvault_name
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = "00000000-0000-0000-0000-000000000000"
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
+}
+
+resource "azurerm_key_vault_secret" "secret" {
+  name         = var.secret_name
+  value        = var.secret_value
+  key_vault_id = azurerm_key_vault.kv.id
+}
